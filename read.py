472b362f4327605318586a492d1b41e8425261ac
Joker2U <yngwieli10@gmail.com>
2026-03-24 22:21:34 +0800

    版本v2

--- read.py
+++ read.py
@@ -4,8 +4,13 @@ with open(r'C:\Users\yngwi\OneDrive\桌面\reviews_analytics\reviews.txt','r') a
     for line in f:
         data.append(line)
         count += 1
-        if count % 1000 == 0:
+        if count % 10000 == 0:
             print(len(data))
-print(len(data))
-print("--------------------")
-print(data[0])
+print("檔案讀取完,總共有",len(data),"筆資料")
+
+sum_len = 0
+for line2 in data:
+    sum_len += len(line2)
+print("平均長度",sum_len / len(data))
+    
+
